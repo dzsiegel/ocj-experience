f9ee26daab2fa205457d0cb38b488361f776a3b2
dzsiegel <36822465+dzsiegel@users.noreply.github.com>
2022-03-09 12:00:29 -0800

    Version 1.0 commit

--- resources/readMore.js
+++ resources/readMore.js
@@ -1,39 +1,36 @@
-//This code is borrowed from someone else and ended up not working properly,
-//because it turned my CSS dysfunctional...I leave it here for ideas
+// code produced by John Margis
 
-//this was the other button that worked perfectly
-//https://www.w3schools.com/howto/howto_js_read_more.asp
-//however, it would only work for one button...
-
+var moreButton = '<button class="readMore" onClick="showMore(this)">Read more</button>';
+var lessButton = '<button class="readMore" onClick="showMore(this)">Read less</button>';
 function AddReadMore() {
   //This limit you can set after how much characters you want to show Read More.
-  var carLmt = 200;
-  // Text to show when text is collapsed
-  var readMoreTxt = "Read More";
-  // Text to show when text is expanded
-  var readLessTxt = " Read Less";
+  var carLmt = 300;
 
-
-  //Traverse all selectors with this class and manupulate HTML part to show Read More
-  $(".addReadMore").each(function() {
-      if ($(this).find(".firstSec").length)
-          return;
-
-      var allstr = $(this).text();
-      if (allstr.length > carLmt) {
-          var firstSet = allstr.substring(0, carLmt);
-          var secdHalf = allstr.substring(carLmt, allstr.length);
-          var strtoadd = firstSet + "<span class='SecSec'>" + secdHalf + "</span><span class='readMore'  title='Click to Show More'>" + readMoreTxt + "</span><span class='readLess' title='Click to Show Less'>" + readLessTxt + "</span>";
-          $(this).html(strtoadd);
-      }
-
-  });
-  //Read More and Read Less Click Event binding
-  $(document).on("click", ".readMore,.readLess", function() {
-      $(this).closest(".addReadMore").toggleClass("showlesscontent showmorecontent");
-  });
+  var text = document.getElementsByClassName("readMore");
+  for(let i=0; i<text.length; i++) {
+    if(text[i].innerText.length > carLmt) {
+      var beforeText = text[i].innerHTML;
+      //console.log(beforeText);
+      let displayText = beforeText.substring(0, carLmt);
+      text[i].setAttribute("moretext",beforeText);
+      text[i].setAttribute("lesstext",displayText);
+      text[i].setAttribute("minimized", true);
+      text[i].innerHTML=displayText + "...\n" + moreButton;
+    }
+  }
 }
-$(function() {
-  //Calling function after Page Load
-  AddReadMore();
-});
+
+function showMore(elem){
+  var node = elem.parentNode.parentNode;
+  console.log(node.getAttribute("lesstext"));
+  if(node.getAttribute("minimized")) {
+    node.innerHTML = node.getAttribute("moretext") + "..." + lessButton;
+    node.setAttribute("minimized", false);
+  }
+  else {
+    var node = elem.parentNode;
+    node.innerHTML = node.getAttribute("lesstext") + "..." + moreButton;
+    node.setAttribute("minimized", true);
+  }
+
+}
